03cdd46ad7e150af42b25e0d52e229e827b3dc3c
Lucas Lima <lucaslimaolft@gmail.com>
2026-09-24 11:16:22 -0300

    primeira postagem

---
+++ app.py
@@ -0,0 +1,17 @@
+#Projeto de exemplo: Calculadora de media do aluno
+def calcular_media(nota1, nota2):
+    return(nota1+nota2)/2
+
+print("===Sistema de Notas do Aluno===")
+
+n1=float(input("Digite a primeira nota: "))
+n2=float(input("Digite a segunda nota: "))
+
+media=calcular_media(n1,n2)
+
+print(f"A média final é: {media:.2f}")
+
+if media >=7:
+    print("Status: APROVADO!")
+else:
+    print("Status:REPROVADO!")
